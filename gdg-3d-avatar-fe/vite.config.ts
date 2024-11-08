2e410b72b1005147394a56ca7dabd3d17b5feba7
junhyun bang <flintintoe@gmail.com>
2024-11-08 13:56:07 +0900

    fix: update vite.config.ts, update  file structure

--- gdg-3d-avatar-fe/vite.config.ts
+++ gdg-3d-avatar-fe/vite.config.ts
@@ -1,14 +1,24 @@
+// vite.config.ts
+
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = path.dirname("./src");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        create: 'create/create.html',
+        main: path.resolve(__dirname, 'index.html'),
+        create: path.resolve(__dirname, 'create/index.html'),
       },
     },
   },
