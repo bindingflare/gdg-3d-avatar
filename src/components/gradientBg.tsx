7a0b0af2232fed9cb0bd2d962d9024ad8159a9d0
junhyun bang <flintintoe@gmail.com>
2024-12-04 03:46:55 +0900

    fix: bg text misplaced

--- src/components/gradientBg.tsx
+++ src/components/gradientBg.tsx
@@ -68,22 +68,22 @@ const GradientBackground: FC<GradientBackgroundProps> = ({
       }}
       onMouseMove={handleMouseMove}
     >
-      <div className="text-white/25 text-center fixed w-full">
+      <div className="text-white/25 mt-[70vh] text-center fixed w-full">
         <Wrapper>
           <div
             className={`opacity-animation relative min-h-[15vh] min-w-full ${
               isTextVisible ? "" : "opacity-0"
             }`}
           >
-            <h2 className="text-3xl sm:text-5xl md:text-7xl italic font-semibold mt-[70vh]">{text}</h2>
+            <h2 className="text-3xl sm:text-5xl md:text-7xl italic font-semibold">{text}</h2>
           </div>
           <div
-            className={`opacity-animation absolute min-h-[15vh] ${
+            className={`opacity-animation absolute min-h-[15vh] max-w-full ${
               isMarqueeVisible ? "" : "opacity-0"
             }`}
           >
-            <Marquee speed={30}>
-              <h2 className="text-7xl italic font-semibold overflow-hidden text-nowrap">
+            <Marquee autoFill={true} speed={30}>
+              <h2 className="text-7xl italic font-semibold overflow-hidden text-nowrap min-h-[15vh]">
                 {text}
               </h2>
             </Marquee>
